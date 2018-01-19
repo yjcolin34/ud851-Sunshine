f04b5564a44fc0db757277f2f669baf60eb0f9e2
201511045 <yjflore34@gmail.com>
2018-01-19 10:57:00 +0900

    add dummy weather text

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -31,7 +31,21 @@ public class MainActivity extends AppCompatActivity {
         // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
         weatherDisplay = (TextView)findViewById(R.id.tv_weather_data);
         // COMPLETED (3) Create an array of Strings that contain fake weather data
-        String weather [] = {"Sunny", "Cloudy", "Rainny"} ;
+        String weather [] = {
+
+                "Tonight: Scattered snow showers, mainly before 9pm.",
+                "Friday: Partly sunny, with a high near 20. North wind 10 to 15 mph.",
+                "Friday Night: Partly cloudy, with a low around -2. North wind around 15 mph.",
+                "Saturday: Sunny, with a high near 3. North wind around 15 mph, with gusts as high as 25 mph.",
+                "Saturday Night: Mostly clear, with a low around -8.",
+                "Sunday: Mostly sunny, with a high near 4.",
+                "Sunday Night: Mostly cloudy, with a low around -4.",
+                "Monday: Partly sunny, with a high near 4.",
+                "Monday Night: Partly cloudy, with a low around -5.",
+                "Tuesday: Mostly sunny, with a high near 6.",
+                "Tuesday Night: A chance of snow showers. Partly cloudy, with a low around -4.",
+                "Wednesday: A chance of snow. Mostly sunny, with a high near 7.",
+} ;
         // COMPLETED (4) Append each String from the fake weather data array to the TextView
         for(String text : weather)
             weatherDisplay.append(text+"\n\n\n");
